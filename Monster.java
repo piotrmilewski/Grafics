0639aad8f5e36a75e581bc7cc85391216ad4cb8a
Yuyang Zhang <Yuyang@Yuyangs-MacBook-Air.local>
2017-01-11 16:46:10 -0500

    Comments for Monster

--- Monster.java
+++ Monster.java
@@ -1,11 +1,11 @@
 import util.java.ArrayList;
 public abstract class Monster extends Monster{
-    public String dropped(ArrayList<String> a){
+    public String dropped(ArrayList<String> a){ //Assuming that monster has an inventory of 3 items, each have an equal chance of being dropped
 	if (Math.random()*10 > 6){
-	    return a.get(1);
+	    return a.get(1); //first item in inventory
 	}
 	else if (Math.random()*10 > 3){
-	    return a.get(2);
+	    return a.get(2); 
 	}
 	else{
 	    return a.get(3);
