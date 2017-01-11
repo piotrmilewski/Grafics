a7e5cdffb0e53f7bf9a319497600550ce89fe610
Yuyang Zhang <yzhang4@stuy.edu>
2017-01-11 07:41:41 -0500

    Updated Monster.java

--- Monster.java
+++ Monster.java
@@ -1,1 +1,35 @@
+public abstract class Monster{
+    protected int _health;
+    protected int _attack;
+    protected int _defense;
+    protected int _speed;
+    protected int _level;
+    protected int _currency;
+    protected ArrayList<String> _inventory;
+
+    public int getHealth(){
+	return _health;
+    }
+
+    public boolean isAlive(){
+	return (getHealth() > 0);
+    }
+
+    public int lowerHP(int damage){
+	if ((_health - damage) > 0){
+	   _health -= damage;
+	}
+	else{
+	    _health = 0;
+	}
+	return _health;
+    }
+
+    public String attack(Character a){
+	
+
+
+
+	
+ 
 
