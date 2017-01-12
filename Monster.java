668fcf9a35fcfd327a13d88a08bb1f3da6d4e4ee
Yuyang Zhang <yzhang4@stuy.edu>
2017-01-12 07:42:36 -0500

    Fillexd ArrayList

--- Monster.java
+++ Monster.java
@@ -1,3 +1,29 @@
+import java.util.ArrayList;
+public abstract class Monster extends LivingEntity{
+    protected ArrayList<String> _inventory;
+
+    public ArrayList<String> fillArrayList(ArrayList<String> _data){
+    for (int x = 0; x < 3; x+=1){
+        _data.add("");
+    }
+    return _data;
+    }
+        
+    
+    public String dropped(ArrayList<String> a){ //Assuming that monster has an inventory of 3 items, each have an equal chance of being dropped
+    if (Math.random()*10 > 6){
+        return a.get(1); //first item in inventory
+    }
+    else if (Math.random()*10 > 3){ //second item
+        return a.get(2);
+    }
+    else{ //third item
+        return a.get(3);
+    }
+    }
+}
+    
+
 import util.java.ArrayList;
 public abstract class Monster extends Monster{
     public String dropped(ArrayList<String> a){ //Assuming that monster has an inventory of 3 items, each have an equal chance of being dropped
@@ -18,4 +44,31 @@ public abstract class Monster extends Monster{
 
 	
  
+
+import java.util.ArrayList;
+public abstract class Monster extends LivingEntity{
+    protected ArrayList<String> _inventory;
+
+    public ArrayList<String> fillArrayList(ArrayList<String> _data){
+    for (int x = 0; x < 3; x+=1){
+        _data.add("");
+    }
+    return _data;
+    }
+        
+    
+    public String dropped(ArrayList<String> a){ //Assuming that monster has an inventory of 3 items, each have an equal chance of being dropped
+    if (Math.random()*10 > 6){
+        return a.get(1); //first item in inventory
+    }
+    else if (Math.random()*10 > 3){ //second item
+        return a.get(2);
+    }
+    else{ //third item
+        return a.get(3);
+    }
+    }
+}
+    
+
 
