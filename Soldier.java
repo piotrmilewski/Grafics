a32e1b7b010257d54e358a2bc7d69cfaf4cffdfd
Angel <Angel>
2017-01-12 23:55:46 -0500

    Created Solider.java

---
+++ Soldier.java
@@ -0,0 +1,14 @@
+public class Soldier{
+    public Soldier(String name){
+	_name = name;
+	_level = 0;
+	_experience = 0;
+	_maxhealth = 200;
+	_health = 200;
+	_attack = 10;
+	_defense = 10;
+	_speed = 10;
+	_spattack = 10;
+	_currency = 0;
+    }
+}
