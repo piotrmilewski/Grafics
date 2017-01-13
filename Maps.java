a799fc3e9dd0e08e5a633b0ab25c646861fef8b9
Yuyang Zhang <yzhang4@stuy.edu>
2017-01-12 23:16:03 -0500

    Updated Maps.java

--- Maps.java
+++ Maps.java
@@ -1,2 +1,7 @@
-public class Maps{
+public abstract class Maps{
+    protected int _spawnrate;
+    protected int _waves;
+
+    public abstract void spawn (Monster m);
+    public abstract String quit();
 }
