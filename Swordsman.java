185e566cea1895b5e86054829fca702a864c4747
piotrmilewski <pmilewski@stuy.edu>
2017-01-15 21:44:21 -0500

    Added Swordsman subclass

---
+++ Swordsman.java
@@ -0,0 +1,14 @@
+public class Swordsman extends Character{
+
+    public Swordsman(String name){
+	_name = name;
+	_level = 0;
+	_experience = 0;
+	_maxhealth = 200;
+	_health = 200;
+	_attack = 10;
+	_defense = 10;
+	_speed = 5;
+	_currency = 0;
+    }
+}
