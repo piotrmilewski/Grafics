43c25d6410070f02388eac3757e67eb981d01788
Yuyang Zhang <yzhang4@stuy.edu>
2017-01-16 00:02:41 -0500

    Created Goblins.java

---
+++ Goblins.java
@@ -0,0 +1,7 @@
+public class Goblins extends Monsters{
+    protected _health = 20;
+    protected _attack = 10;
+    protected _defense = 1;
+    protected _speed = 10;
+    protected _currency = 5;
+}
