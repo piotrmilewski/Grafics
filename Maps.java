8fb2b83245db515b81a8d21b91eb536c4b82cb52
Yuyang Zhang <yzhang4@stuy.edu>
2017-01-15 22:50:59 -0500

    Created quit

--- Maps.java
+++ Maps.java
@@ -2,7 +2,7 @@ public abstract class Maps{
     protected Character player;
     protected Monster monster;
     
-    public void spawn (){
+    public void spawn(){
 	if (Math.random()*10 > 6){
 	    monster = new Gargoyle();
 	}
@@ -15,39 +15,48 @@ public abstract class Maps{
     }
 
     public void fight(){
+	boolean quit = false;
 	while (player.isAlive() && monster.isAlive()){
 	    prints = "Would you like to attack or parry the next monster's attack during your next turn?\n";
-	    prints += "Selection (attack or parry): ";
+	    prints += "Selection (attack(1) or parry(2) or would you like to quit(3)?): ";
 	    System.out.print(prints);
-	    selection = Keyboard.readString();
+	    int selection = Keyboard.readInt();
 
-	    if (selection.equals("attack")){
+	    if (selection.equals(1)){
 		dP = player.attack(monster);
 	    }
-	    else{
+	    else if (selection.equals(2)){
 		dP = player.parry(monster);
 	    }
-	    dM = monster.attack(player);
+	    else{
+		quit = true;
+	        break;
+	    }
+	}	    		
+	dM = monster.attack(player);
 
-	    prints = "\n" + player.getName() + " dealt " + dP + " damage to the Gargoyle.\n";
-	    prints += "Gargoyle dealt " + dM + " damage to " + player.getName() + ".\n";
-	    prints += "Your health is now: " + player.getHealth() + "\n";
-	    prints += "Gargoyle's health is now: " + monster.getHealth() + "\n";
-	    System.out.println(prints);
-	}
+	prints = "\n" + player.getName() + " dealt " + dP + " damage to the Gargoyle.\n";
+	prints += "Gargoyle dealt " + dM + " damage to " + player.getName() + ".\n";
+	prints += "Your health is now: " + player.getHealth() + "\n";
+	prints += "Gargoyle's health is now: " + monster.getHealth() + "\n";
+	System.out.println(prints);
+    }
 
-	if (!player.isAlive() && !monster.isAlive()){
-	    prints = "Although " + player.getName() + " killed the monster with one last blow from his fist, the monster lashed at " + player.getName() + " with one last slash and took " + player.getName() + " down with him.\n";
-	    System.out.println(prints);
-	}
-	else if (!monster.isAlive()){
-	    prints = "With one last blow from his fist, " + player.getName() + " was able to take down the monster\n";
-	    System.out.println(prints);
-	}
-	else if (!player.isAlive()){
-	    prints = "With one last slash from its claw, the monster was able to take down the mighty " + player.getName() + "\n";
-	    System.out.println(prints);
-	}	
-    public abstract String quit();
+    if (quit){
+	prints = "PLAYER IS A LOSER, HE RAN BACK TO THE CASTLE AND DROPPED SOME STUFF AND MONEY";
+    }
+    else if (!player.isAlive() && !monster.isAlive()){
+        prints = "Although " + player.getName() + " killed the monster with one last blow from his fist, the monster lashed at " + player.getName() + " with one last slash and took " + player.getName() + " down with him.\n";
+        System.out.println(prints);
+    }
+    else if (!monster.isAlive()){
+        prints = "With one last blow from his fist, " + player.getName() + " was able to take down the monster\n";
+        System.out.println(prints);
+    }
+    else if (!player.isAlive()){
+        prints = "With one last slash from its claw, the monster was able to take down the mighty " + player.getName() + "\n";
+        System.out.println(prints);
+    }
+    Castle();
    
 }
