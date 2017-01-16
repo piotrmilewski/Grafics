35e3695e6fe10149ebdb17662fba71ffe4a1056b
piotrmilewski <pmilewski@stuy.edu>
2017-01-16 16:59:32 -0500

    Removed extra instance variables from Goblins

--- Goblins.java
+++ Goblins.java
@@ -1,9 +1,4 @@
 public class Goblins extends Monsters{
-    protected _health = 20;
-    protected _attack = 10;
-    protected _defense = 1;
-    protected _speed = 10;
-    protected _currency = 5;
 
     public Goblins(){
 	_health = 20;
