187e65efdccf7fcbebf49e8d21c7d9abb788725f
Yuyang Zhang <yzhang4@stuy.edu>
2017-01-17 07:25:53 -0500

    Updated Quest1.java

--- Quest1.java
+++ Quest1.java
@@ -2,31 +2,28 @@ import cs1.Keyboard;
 public class Quest1 extends Maps{	    
     public  void newQuest(){
 	String prints;
-	int selection = Keyboard.readString();
+	String selection;
 	prints = "Welcome to the Caves of Anarachy. Here you will begin your first quest.\n";
 	prints += "Are you sure you would like to continue? (yes) or (no)";
 	System.out.println(prints);
-	if (selection.Lowercase() == "yes"){		
+	selection = Keyboard.readString();
+	if (selection.toLowerCase() == "yes"){		
 	    prints = "As any brave fighter, a weapon must be needed (Press enter to continue)\n";
-	    System.out.println(prints);   
-	    if (selection.Lowercase() == ""){
-	    }
+	    System.out.println(prints);  
+	    selection = Keyboard.readString(); 
 	    prints = "Here, within the Caves of Anarachy, you will be able to find your chosen weapon (Press enter to continue)\n";
 	    System.out.println(prints);
-	    if (selection.Lowercase() == ""){
-	    }
-	    prints = "However, the Caves of Anarachy is a rather dangerous area, with many monsters hidden (Press enter to continue)\n";
+	    selection = Keyboard.readString();
+	    prints = "However, the Caves of Anarachy is a rather dangerous area, where many monsters hidden (Press enter to continue)\n";
 	    System.out.println(prints);
-	    if (selection.Lowercase() == ""){
-	    }
+	    selection = Keyboard.readString();
 	    prints = "To your left, watch out!\n";
 	    System.out.println(prints);		
 	    spawn();
 	    fight();
 	    prints = "That was a rather close one, now wasn't it (Press enter to continue)\n";
 	    System.out.println(prints);
-	    if (selection.Lowercase() == ""){
-	    }
+	    selection = Keyboard.readString();
 	    prints += "Oh no another one!\n";
 	    System.out.println(prints);
 	    spawn();
