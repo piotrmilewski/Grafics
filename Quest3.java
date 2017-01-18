3dedb456bc9ea61e9653c75f98529212e7a1aed4
Yuyang Zhang <yzhang4@stuy.edu>
2017-01-18 08:40:25 -0500

    Created placeholders

--- Quest3.java
+++ Quest3.java
@@ -3,28 +3,28 @@ public class Quest3 extends Maps{
     public boolean void newQuest(){
 	String prints;
 	String selection;
-	prints = "Welcome to the Caves of Anarachy. Here you will begin your first quest.\n";
+	prints = "Welcome to the Chicken. Here you will begin your third quest.\n";
 	prints += "Are you sure you would like to continue? (yes) or (no)";
 	System.out.println(prints);
 	selection = Keyboard.readString();
 	if (selection.toLowerCase() == "yes"){
-	    prints = "As any brave fighter, a weapon must be needed (Press enter to continue)\n";
+	    prints = "eh heuid gow odher ubhoal eijfi jn  (Press enter to continue)\n";
 	    System.out.println(prints);  
 	    selection = Keyboard.readString(); 
-	    prints = "Here, within the Caves of Anarachy, you will be able to find your chosen weapon (Press enter to continue)\n";
+	    prints = "eh heuid gow odher ubhoal eijfi jn  (Press enter to continue)\n";
 	    System.out.println(prints);
 	    selection = Keyboard.readString();
-	    prints = "However, the Caves of Anarachy is a rather dangerous area, where many monsters hidden (Press enter to continue)\n";
+	    prints = "eh heuid gow odher ubhoal eijfi jn (Press enter to continue)\n";
 	    System.out.println(prints);
 	    selection = Keyboard.readString();
 	    prints = "To your left, watch out!\n";
 	    System.out.println(prints);
 	    spawn();
 	    fight();
-	    prints = "That was a rather close one, now wasn't it (Press enter to continue)\n";
+	    prints = "eh heuid gow odher ubhoal eijfi jn  (Press enter to continue)\n";
 	    System.out.println(prints);
 	    selection = Keyboard.readString();
-	    prints += "Oh no another one!\n";
+	    prints += "eh heuid gow odher ubhoal eijfi jn \n";
 	    System.out.println(prints);
 	    spawn();
 	    fight();
