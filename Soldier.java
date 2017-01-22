bbf22c29f2875bf060c1100fd863703eb63f9ca2
Angel <ang7@stuy.edu>
2017-01-22 14:03:19 -0500

    Modified constructor

--- Soldier.java
+++ Soldier.java
@@ -2,14 +2,14 @@ public class Soldier extends Character{
     
     public Soldier(String name){
 	_name = name;
-	_level = 0;
+	_level = 1;
 	_experience = 0;
 	_maxhealth = 200;
 	_health = 200;
 	_attack = 10;
 	_defense = 7;
 	_speed = 10;
-	_currency = 0;
+	_currency = 50;
 	fillInv();
     }
 }
