1495ca3f58c5bc87a106623094da7c20f6ed3d36
Angel <ang7@stuy.edu>
2017-01-22 11:13:31 -0500

    Modified forest()

--- Forest.java
+++ Forest.java
@@ -5,20 +5,28 @@ public class Forest {
 	String prints;
 	int selection;
 	prints = "Welcome to the forest! Sometimes I come here to relax 'cause it's beautiful here.";
-	prints += "\nWould you like to enter? Yes(1) No(2)";
+	prints += "\nWould you like to enter? 1: Yes 2: No";
 	System.out.println(prints);
 	selection = Keyboard.readInt();
 	if (selection == 1){	  	
 	    while (Woo.getPlayer().isAlive()){
 		woo.spawn();
 		woo.fight();
-		prints = "Would you like to continue? Yes(1) No(2)";
+		prints = "Would you like to continue? 1: Yes 2: No";
 		System.out.println(prints);
 		selection = Keyboard.readInt();
 		if (selection == 2){
 		    break;
 		}
 	    }    
+	}
+	else if (selection == 2){
+	    return;
+	}
+	else{
+	    System.out.print("That isn't one of the choices...\n");
+	    System.out.print(prints);
+	    selection = Keyboard.readInt();
 	}
     }//end forest()
 }
