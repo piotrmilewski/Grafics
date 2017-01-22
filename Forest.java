700b38a215cbf9aa1a551fc417c666ec2251068b
Angel <ang7@stuy.edu>
2017-01-22 16:26:49 -0500

    Allowed player to use items

--- Forest.java
+++ Forest.java
@@ -4,6 +4,7 @@ public class Forest {
     public void forest(){
 	String prints;
 	int selection;
+	Character player = Woo.getPlayer();
 	prints = "Welcome to the forest! Sometimes I come here to relax 'cause it's beautiful here.";
 	prints += "\nWould you like to enter? 1: Yes 2: No";
 	System.out.println(prints);
@@ -12,13 +13,24 @@ public class Forest {
 	    while (Woo.getPlayer().isAlive()){
 		woo.spawn();
 		woo.fight();
-		prints = "Would you like to continue? 1: Yes 2: No";
+		prints = "What would you like to do? 1: Keep going! 2: Leave! 3: Use an item!";
 		System.out.println(prints);
 		selection = Keyboard.readInt();
 		if (selection == 2){
 		    break;
 		}
-	    }    
+		else if (selection == 3){
+		    System.out.print(player.printInventory());
+		    System.out.print("\nWhich item would you like to use?");
+		    String item = Keyboard.readString().toLowerCase();
+		    if (player.hasItem(item)){
+			player.useItem(item);
+		    }
+		    else{
+			System.out.print("\nYou don't that item. Try visiting the shop!");
+		    }
+		}    
+	    }
 	}
 	else if (selection == 2){
 	    return;
