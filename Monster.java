aeb1d53ff722c7a011d75a2398a33c536014a92c
Angel <ang7@stuy.edu>
2017-01-22 18:53:46 -0500

    Modified fillItems() and drop()

--- Monster.java
+++ Monster.java
@@ -2,35 +2,33 @@ import java.util.ArrayList;
 public abstract class Monster extends LivingEntity{
     protected ArrayList<String> _inventory;
 
-    public ArrayList<String> fillArrayList(ArrayList<String> _data){
+    public void fillItems(){
+	_inventory = new ArrayList<String>();
 	for (int x = 0; x < 3; x+=1){
-		      if (Math.random()*3 > 2.01){
-			  _data.add("health_potion");
-		      }
-		      else if (Math.random()*3 == 2){
-			  _data.add("magic_dulce");
-			      }
-		      else if (Math.random()*3 > 1){
-			  _data.add("attack_potion");
-		      }
-		     
-		      else{
-			  _data.add("defense_potion");
-		      }
+	    if (Math.random() < 0.25){
+		_inventory.add("Potion");
+	    }
+	    else if (Math.random()*3 < .5){
+		_inventory.add("Rare Sweet");
+	    }
+	    else if (Math.random()*3 < .75){
+		_inventory.add("AttackUp");
+	    }
+	    else{
+		_inventory.add("DefenseUp");
+	    }
 	}		      
-	return _data;
-    }
-        
+    }    
     
-    public String dropped(ArrayList<String> a){ //Assuming that monster has an inventory of 3 items, each have an equal chance of being dropped
+    public String drop(){ //Assuming that monster has an inventory of 3 items, each have an equal chance of being dropped
 	if (Math.random()*10 > 6){
-	    return a.get(1); //first item in inventory
+	    return _inventory.get(0); //first item in inventory
 	}
 	else if (Math.random()*10 > 3){ //second item
-	    return a.get(2);
+	    return _inventory.get(1);
 	}
 	else{ //third item
-	    return a.get(3);
+	    return _inventory.get(2);
 	}
     }
 }
