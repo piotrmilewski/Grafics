df2a69cb7f428d12fc110984c927e7a2cb3fc332
Yuyang Zhang <yzhang4@stuy.edu>
2017-01-22 19:40:42 -0500

    Updated health

--- Soldier.java
+++ Soldier.java
@@ -4,8 +4,8 @@ public class Soldier extends Character{
 	_name = name;
 	_level = 1;
 	_experience = 0;
-	_maxhealth = 200;
-	_health = 200;
+	_maxhealth = 175;
+	_health = 175;
 	_attack = 10;
 	_defense = 7;
 	_currency = 50;
