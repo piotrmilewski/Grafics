6a6e6f4a41d53bda2dd00f60567b264cc56aac00
Yuyang Zhang <yzhang4@stuy.edu>
2017-01-23 02:18:33 -0500

    Updated Swordsman.java

--- Swordsman.java
+++ Swordsman.java
@@ -2,13 +2,13 @@ public class Swordsman extends Character{
 
     public Swordsman(String name){ //creates Swordman
 	_name = name;
-	_level = 0;
+	_level = 1;
 	_experience = 0;
 	_maxhealth = 200;
 	_health = 200;
 	_attack = 10;
 	_defense = 9;
-	_currency = 0;
+	_currency = 50;
 	fillInv();
     }
 }
